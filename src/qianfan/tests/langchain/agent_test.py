a2a45fa1e38553c58f77b8ece3ea995ecabccfa7
Dobiichi-Origami <56953648+Dobiichi-Origami@users.noreply.github.com>
2023-10-25 11:42:42 +0800

    make unit test conditional on specific aspect

--- src/qianfan/tests/langchain/agent_test.py
+++ src/qianfan/tests/langchain/agent_test.py
@@ -1,7 +1,5 @@
+import sys
 import pytest
-from langchain.agents import AgentExecutor
-from langchain.chat_models import QianfanChatEndpoint
-from langchain.tools import tool
 
 from qianfan.extensions.langchain.agents import (
     QianfanMultiActionAgent,
@@ -9,40 +7,49 @@ from qianfan.extensions.langchain.agents import (
 )
 
 
-@tool
-def paper_search(query: str, **kwargs) -> str:
-    """using search engine to retrieve papers about your query"""
-    if not query == "physics":
-        raise ValueError(
-            f"value retrieved from mock server isn't physics, rather: {query}"
-        )
+def get_tool_list():
+    from langchain.tools import tool
 
-    return """
-Published: 2012-09-04
-Title: Is Physics Sick? [In Praise of Classical Physics]
-Authors: Hisham Ghassib
-Summary: In this paper, it is argued that theoretical physics is more akin to an
-organism than to a rigid structure.It is in this sense that the epithet,
-"sick", applies to it. It is argued that classical physics is a model of a
-healthy science, and the degree of sickness of modern physics is measured
-accordingly. The malady is located in the relationship between mathematics and
-physical meaning in physical theory.
+    @tool
+    def paper_search(query: str, **kwargs) -> str:
+        """using search engine to retrieve papers about your query"""
+        if not query == "physics":
+            raise ValueError(
+                f"value retrieved from mock server isn't physics, rather: {query}"
+            )
 
-Published: 2000-02-10
-Title: Modern Mathematical Physics: what it should be?
-Authors: Ludwig Faddeev
-Summary: Personal view of author on goals and content of Mathematical Physics.
+        return """
+    Published: 2012-09-04
+    Title: Is Physics Sick? [In Praise of Classical Physics]
+    Authors: Hisham Ghassib
+    Summary: In this paper, it is argued that theoretical physics is more akin to an
+    organism than to a rigid structure.It is in this sense that the epithet,
+    "sick", applies to it. It is argued that classical physics is a model of a
+    healthy science, and the degree of sickness of modern physics is measured
+    accordingly. The malady is located in the relationship between mathematics and
+    physical meaning in physical theory.
 
-Published: 2005-03-15
-Title: Topology in Physics
-Authors: R. Jackiw
-Summary: The phenomenon of quantum number fractionalization is explained. The
-relevance of non-trivial phonon field topology is emphasized.
-    """
+    Published: 2000-02-10
+    Title: Modern Mathematical Physics: what it should be?
+    Authors: Ludwig Faddeev
+    Summary: Personal view of author on goals and content of Mathematical Physics.
+
+    Published: 2005-03-15
+    Title: Topology in Physics
+    Authors: R. Jackiw
+    Summary: The phenomenon of quantum number fractionalization is explained. The
+    relevance of non-trivial phonon field topology is emphasized.
+        """
+
+    return [paper_search]
 
 
+@pytest.mark.skipif(sys.version_info < (3, 8, 1), reason="requires Python 3.8.1 or higher")
 def test_sync_qianfan_single_agent():
-    tools = [paper_search]
+    from langchain.agents import AgentExecutor
+    from langchain.chat_models import QianfanChatEndpoint
+
+    tools = get_tool_list()
     agent_qianfan = QianfanSingleActionAgent.from_system_prompt(
         tools, QianfanChatEndpoint(model="ERNIE-Bot-4")
     )
@@ -50,9 +57,13 @@ def test_sync_qianfan_single_agent():
     assert agent_executor_qianfan.run("帮我搜索一篇 physics 领域的论文") == "测试成功"
 
 
+@pytest.mark.skipif(sys.version_info < (3, 8, 1), reason="requires Python 3.8.1 or higher")
 @pytest.mark.asyncio
 async def test_async_qianfan_single_agent():
-    tools = [paper_search]
+    from langchain.agents import AgentExecutor
+    from langchain.chat_models import QianfanChatEndpoint
+
+    tools = get_tool_list()
     agent_qianfan = QianfanSingleActionAgent.from_system_prompt(
         tools, QianfanChatEndpoint(model="ERNIE-Bot-4")
     )
@@ -62,8 +73,12 @@ async def test_async_qianfan_single_agent():
     ) == "测试成功"
 
 
+@pytest.mark.skipif(sys.version_info < (3, 8, 1), reason="requires Python 3.8.1 or higher")
 def test_sync_qianfan_multi_agent():
-    tools = [paper_search]
+    from langchain.agents import AgentExecutor
+    from langchain.chat_models import QianfanChatEndpoint
+
+    tools = get_tool_list()
     agent_qianfan = QianfanMultiActionAgent.from_system_prompt(
         tools, QianfanChatEndpoint(model="ERNIE-Bot-4")
     )
@@ -71,9 +86,13 @@ def test_sync_qianfan_multi_agent():
     assert agent_executor_qianfan.run("帮我搜索一篇 physics 领域的论文") == "测试成功"
 
 
+@pytest.mark.skipif(sys.version_info < (3, 8, 1), reason="requires Python 3.8.1 or higher")
 @pytest.mark.asyncio
 async def test_async_qianfan_multi_agent():
-    tools = [paper_search]
+    from langchain.agents import AgentExecutor
+    from langchain.chat_models import QianfanChatEndpoint
+
+    tools = get_tool_list()
     agent_qianfan = QianfanMultiActionAgent.from_system_prompt(
         tools, QianfanChatEndpoint(model="ERNIE-Bot-4")
     )

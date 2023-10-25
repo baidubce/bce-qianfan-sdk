4f0973d9ec002ecc8f0549fed4c0b617e2481fb1
Dobiichi-Origami <56953648+Dobiichi-Origami@users.noreply.github.com>
2023-10-25 16:02:06 +0800

    add cookbook about agent

--- src/qianfan/extensions/langchain/agents/__init__.py
+++ src/qianfan/extensions/langchain/agents/__init__.py
@@ -1,4 +1,4 @@
-from .qianfan_agent.baidu_qianfan_endpoint import (
+from qianfan.extensions.langchain.agents.baidu_qianfan_endpoint import (
     QianfanMultiActionAgent,
     QianfanSingleActionAgent,
 )

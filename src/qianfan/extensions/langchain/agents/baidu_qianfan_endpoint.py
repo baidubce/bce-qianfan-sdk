4187f79761243bb0fc131cd126f1851db266b83b
Dobiichi-Origami <56953648+Dobiichi-Origami@users.noreply.github.com>
2023-11-01 18:28:38 +0800

    add notice comment

--- src/qianfan/extensions/langchain/agents/baidu_qianfan_endpoint.py
+++ src/qianfan/extensions/langchain/agents/baidu_qianfan_endpoint.py
@@ -28,7 +28,14 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
     MessagesPlaceholder,
 )
-from langchain.pydantic_v1 import BaseModel, root_validator
+
+# notice:
+# 此处使用 langchain.pydantic_v1 是 langchain 依赖 pydantic 1.x 的兼容手段
+# 使 SDK 安装了 pydantic 2.x 时，agent 代码也能正常工作。请勿修改。
+from langchain.pydantic_v1 import (
+    BaseModel,
+    root_validator,
+)
 from langchain.schema import (
     AgentAction,
     AgentFinish,

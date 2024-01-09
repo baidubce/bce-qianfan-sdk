0e040efbe799785426b2f2bc03890f69c6d999db
ZingLix <liujun45@baidu.com>
2024-01-09 20:15:05 +0800

    fix chinese character cannot be deleted properly

--- src/qianfan/common/client/main.py
+++ src/qianfan/common/client/main.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# The readline library needs to be imported first to
+# ensure that Chinese input works properly in the client.
+import readline  # noqa
 from typing import Optional
 
 import typer

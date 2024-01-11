2d5cd541a163f09ed0b30930f7cde4c2c11c0647
Dobiichi-Origami <56953648+Dobiichi-Origami@users.noreply.github.com>
2024-01-11 21:23:36 +0800

    add comment

--- src/qianfan/config.py
+++ src/qianfan/config.py
@@ -105,6 +105,9 @@ class GlobalConfig(BaseSettings):
     )
     BOS_HOST_REGION: str = Field(default=DefaultValue.BosHostRegion)
 
+    # Warning
+    # 这个配置项会关闭 SSL 证书校验功能，可能会导致潜在的不安全访问
+    # 请勿在公共网络上关闭这一配置。由于关闭带来的一切问题，本项目均不负责
     SSL_VERIFICATION_ENABLED: bool = Field(default=DefaultValue.SSLVerificationEnabled)
 
 

9522c48f5e1ac92e205ea5c04ef7ced0d28afbdf
zhonghanjun <zhonghanjun@baidu.com>
2024-01-23 22:17:07 +0800

    fix: ut

--- src/scripts/run_test.sh
+++ src/scripts/run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe
 
-poetry run coverage run -m pytest -v -r A --full-trace qianfan/tests/trainer_test.py::test_train1
+poetry run coverage run -m pytest -v -r A --full-trace qianfan
 poetry run coverage report --omit="qianfan/tests/*"

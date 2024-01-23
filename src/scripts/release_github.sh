bffb2c9a84da5a8d4b2f9296c0e205b44d58e7d2
NuODaniel <zhonghanjun@baidu.com>
2024-01-23 23:50:49 +0800

    fix: release script (#227)
    * fix: release script
    
    * Update release_github.sh

--- src/scripts/release_github.sh
+++ src/scripts/release_github.sh
@@ -1,9 +1,9 @@
 #!/bin/bash  
   
-if [[ ! ${TAG_NAME} =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then  
+if [[ ! ${TAG_NAME} =~ ^[a-zA-Z0-9.]+$ ]]; then  
   echo "invalid version tag."   
   exit 1
 fi
 
 # upload to pypi
-poetry publish --build -u __token__ -p ${TOKEN}
+poetry publish --build -u __token__ -p ${TOKEN} 

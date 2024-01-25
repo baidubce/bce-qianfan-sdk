538f4b3b07756bc7d10778edaa91bc4e3abd9e36
ZingLix <liujun45@baidu.com>
2024-01-25 20:31:43 +0800

    update script

--- python/scripts/run_mock_server.sh
+++ python/scripts/run_mock_server.sh
@@ -7,7 +7,7 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 
 cd "${SCRIPTPATH}/../"
 export PYTHONPATH="${SCRIPTPATH}/../"
-nohup python "${SCRIPTPATH}/../qianfan/tests/utils/mock_server.py" > /dev/null 2>&1 & 
+nohup python "${SCRIPTPATH}/../qianfan/tests/utils/mock_server.py" > /tmp/mock_server 2>&1 & 
 
 for i in {1..20}; do 
     curl 127.0.0.1:8866 > /dev/null 2>&1
@@ -18,4 +18,5 @@ for i in {1..20}; do
     sleep 0.5
 done
 echo "Start mock server failed"
+cat /tmp/mock_server
 exit 1

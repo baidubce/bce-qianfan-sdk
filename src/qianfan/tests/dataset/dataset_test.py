e16c8f5763954057dc575298a8c75484f2324afa
Dobiichi-Origami <56953648+Dobiichi-Origami@users.noreply.github.com>
2024-01-25 22:50:16 +0800

    format

--- src/qianfan/tests/dataset/dataset_test.py
+++ src/qianfan/tests/dataset/dataset_test.py
@@ -24,7 +24,6 @@ from qianfan.dataset.consts import QianfanDataGroupColumnName
 from qianfan.dataset.data_source import DataSource, QianfanDataSource
 from qianfan.dataset.dataset import Dataset, FormatType
 from qianfan.dataset.qianfan_data_operators import FilterCheckNumberWords
-from qianfan.dataset.dataset import Dataset
 from qianfan.dataset.schema import (
     QianfanGenericText,
     QianfanNonSortedConversation,

a0460aa8a7e55bc652b2ae02f4805fd57fb73c90
Dobiichi-Origami <56953648+Dobiichi-Origami@users.noreply.github.com>
2024-02-05 14:04:26 +0800

    RPM/TPM Limiter alpha

--- python/qianfan/resources/rate_limiter.py
+++ python/qianfan/resources/rate_limiter.py
@@ -23,6 +23,83 @@ from typing import Any, Optional, Type
 from aiolimiter import AsyncLimiter
 
 from qianfan.config import get_config
+from qianfan.utils import log_error
+
+
+class VersatileRateLimiter:
+    """
+    Implementation of Versatile Rate Limiter.
+    They're different rules between synchronous and asynchronous method using,
+    we recommend only use one of two method within single rate limiter at same time
+    """
+
+    def __init__(
+        self, query_per_second: float = 0, request_per_minute: float = 0, **kwargs: Any
+    ) -> None:
+        if request_per_minute <= 0:
+            request_per_minute = 0
+        if query_per_second <= 0:
+            query_per_second = get_config().QPS_LIMIT
+
+        self.is_closed = request_per_minute <= 0 and query_per_second <= 0
+        if self.is_closed:
+            return
+
+        if request_per_minute > 0 and query_per_second > 0:
+            err_msg = (
+                "can't set both request_per_minute and query_per_second simultaneously"
+            )
+            log_error(err_msg)
+            raise ValueError(err_msg)
+
+        if request_per_minute > 0:
+            self._is_rpm = True
+            self._internal_qp10s_rate_limiter = RateLimiter(request_per_minute / 6, 10)
+            self._internal_rpm_rate_limiter = RateLimiter(request_per_minute, 60)
+
+        if query_per_second > 0:
+            self._is_rpm = False
+            self._internal_qps_rate_limiter = RateLimiter(query_per_second)
+
+    def __enter__(self) -> None:
+        if self.is_closed:
+            return
+
+        if self._is_rpm:
+            with self._internal_rpm_rate_limiter:
+                with self._internal_qp10s_rate_limiter:
+                    return
+        else:
+            with self._internal_qps_rate_limiter:
+                return
+
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
+    ) -> None:
+        return
+
+    async def __aenter__(self) -> None:
+        if self.is_closed:
+            return
+
+        if self._is_rpm:
+            async with self._internal_rpm_rate_limiter:
+                async with self._internal_qp10s_rate_limiter:
+                    return
+        else:
+            async with self._internal_qps_rate_limiter:
+                return
+
+    async def __aexit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
+    ) -> None:
+        return
 
 
 class RateLimiter:
@@ -50,7 +127,7 @@ class RateLimiter:
                 ValueError: A ValueError will be raised if `query_per_period` is smaller
                 than 1 or `period_in_second` isn't positive
             """
-            if not (query_per_period >= 1 and period_in_second > 0):
+            if not (query_per_period >= 0 and period_in_second > 0):
                 raise ValueError(
                     "argument illegal with query_per_period {} and period_in_second {}"
                     .format(query_per_period, period_in_second)
@@ -97,31 +174,33 @@ class RateLimiter:
     def _check_is_closed(self) -> bool:
         return self._is_closed
 
-    def __init__(self, query_per_second: float = 0, **kwargs: Any):
+    def __init__(
+        self, query_per_period: float = 0, period_in_second: float = 1, **kwargs: Any
+    ):
         """
         initialize rate limiter
 
         Args:
-            query_per_second (float): query times in one second, default to 0,
-            meaning rate limiter close.
+            query_per_period (float):
+                query times in one period, default to 0,
+                meaning rate limiter close.
+            period_in_second (float):
+                the length of period in second, default to 1 sec.
+            **kwargs (Any):
+                other keyword arguments
         """
 
-        if query_per_second == 0:
-            query_per_second = get_config().QPS_LIMIT
-
-        self._is_closed = query_per_second <= 0
+        self._is_closed = query_per_period <= 0
         if self._check_is_closed():
             return
-        period_length: float
-        if query_per_second > 1:
-            query_per_period = query_per_second
-            period_length = 1
-        else:
-            query_per_period = 1
-            period_length = 1 / query_per_second
 
-        self._async_limiter = AsyncLimiter(query_per_period, period_length)
-        self._sync_limiter = self._SyncLimiter(query_per_period, period_length)
+        # 向上取整到 1，避免 AsyncLimiter 失效
+        if query_per_period < 1:
+            period_in_second = period_in_second / query_per_period
+            query_per_period = 1
+
+        self._async_limiter = AsyncLimiter(query_per_period, period_in_second)
+        self._sync_limiter = self._SyncLimiter(query_per_period, period_in_second)
 
     def __enter__(self) -> None:
         """

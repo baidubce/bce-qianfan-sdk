f2b2e5ee75aa9ed80196804bcac3569d0a5c809f
Liu Jun <liujun45@baidu.com>
2024-02-22 17:25:15 +0800

    add examples

---
+++ go/examples/list_model/main.go
@@ -0,0 +1,29 @@
+// Copyright (c) 2024 Baidu, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import "github.com/baidubce/bce-qianfan-sdk/go/qianfan"
+
+// 展示了如何获取所有可用的模型
+func main() {
+	// 根据用途选择
+	u := qianfan.NewChatCompletion()
+	// u := qianfan.NewCompletion()
+	// u := qianfan.NewEmbedding()
+
+	for _, m := range u.ModelList() {
+		println(m)
+	}
+}

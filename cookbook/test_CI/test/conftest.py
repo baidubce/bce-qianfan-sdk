620ed5b7958a8bb8cc6189ab00c7f52075c19996
guoweiming <guoweiming@baidu.com>
2024-02-29 15:07:44 +0800

    rm ckbk testCI
    Change-Id: I51d64782580b57b259146d21ae6ae555681d2519

--- cookbook/test_CI/test/conftest.py
+++
@@ -1,5 +0,0 @@
-def pytest_addoption(parser):
-    parser.addoption("--ak", default="")
-    parser.addoption("--sk", default="")
-    parser.addoption("--root-dir", default="..")
-    parser.addoption("--keywords", default="{}")

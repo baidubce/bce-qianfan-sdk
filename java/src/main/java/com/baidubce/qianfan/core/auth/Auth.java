1d6c1ce3e9a6a43d7d7493a14440b86983315490
liyuxuan06 <961523404@qq.com>
2024-03-04 16:27:37 +0800

    refactor: rename package com.baidubce to com.baidubce.qianfan

---
+++ java/src/main/java/com/baidubce/qianfan/core/auth/Auth.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2024 Baidu, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.baidubce.qianfan.core.auth;
+
+import com.baidubce.qianfan.model.exception.ValidationException;
+import com.baidubce.qianfan.util.StringUtils;
+
+public class Auth {
+    public static final String TYPE_IAM = "IAM";
+    public static final String TYPE_OAUTH = "OAuth";
+    public static final String ENV_ACCESS_KEY = "QIANFAN_ACCESS_KEY";
+    public static final String ENV_SECRET_KEY = "QIANFAN_SECRET_KEY";
+    public static final String ENV_QIANFAN_AK = "QIANFAN_AK";
+    public static final String ENV_QIANFAN_SK = "QIANFAN_SK";
+
+    private Auth() {
+    }
+
+    public static IAuth create() {
+        String accessKey = System.getenv(ENV_ACCESS_KEY);
+        String secretKey = System.getenv(ENV_SECRET_KEY);
+        if (StringUtils.isNotEmpty(accessKey) && StringUtils.isNotEmpty(secretKey)) {
+            return create(TYPE_IAM, accessKey, secretKey);
+        }
+        String qianfanAK = System.getenv(ENV_QIANFAN_AK);
+        String qianfanSK = System.getenv(ENV_QIANFAN_SK);
+        if (StringUtils.isNotEmpty(qianfanAK) && StringUtils.isNotEmpty(qianfanSK)) {
+            return create(TYPE_OAUTH, qianfanAK, qianfanSK);
+        }
+        throw new ValidationException("No access key or secret key found in environment variables");
+    }
+
+    public static IAuth create(String accessKey, String secretKey) {
+        return create(TYPE_IAM, accessKey, secretKey);
+    }
+
+    public static IAuth create(String type, String accessKey, String secretKey) {
+        if (TYPE_IAM.equals(type)) {
+            return new IAMAuth(accessKey, secretKey);
+        } else if (TYPE_OAUTH.equals(type)) {
+            return new QianfanOAuth(accessKey, secretKey);
+        } else {
+            throw new ValidationException("Unsupported auth type: " + type);
+        }
+    }
+}
---
+++ java/src/main/java/com/baidubce/qianfan/core/builder/CompletionBuilder.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2024 Baidu, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.baidubce.qianfan.core.builder;
+
+import com.baidubce.qianfan.Qianfan;
+import com.baidubce.qianfan.model.completion.CompletionRequest;
+import com.baidubce.qianfan.model.completion.CompletionResponse;
+import com.baidubce.qianfan.model.constant.ModelEndpoint;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class CompletionBuilder extends BaseBuilder<CompletionBuilder> {
+    private String prompt;
+
+    private Double temperature;
+
+    private Integer topK;
+
+    private Double topP;
+
+    private Double penaltyScore;
+
+    private List<String> stop;
+
+    public CompletionBuilder() {
+        super();
+    }
+
+    public CompletionBuilder(Qianfan qianfan) {
+        super(qianfan);
+    }
+
+    public CompletionBuilder prompt(String prompt) {
+        this.prompt = prompt;
+        return this;
+    }
+
+    public CompletionBuilder temperature(Double temperature) {
+        this.temperature = temperature;
+        return this;
+    }
+
+    public CompletionBuilder topK(Integer topK) {
+        this.topK = topK;
+        return this;
+    }
+
+    public CompletionBuilder topP(Double topP) {
+        this.topP = topP;
+        return this;
+    }
+
+    public CompletionBuilder penaltyScore(Double penaltyScore) {
+        this.penaltyScore = penaltyScore;
+        return this;
+    }
+
+    public CompletionBuilder stop(List<String> stop) {
+        this.stop = stop;
+        return this;
+    }
+
+    public CompletionRequest build() {
+        String finalEndpoint = ModelEndpoint.getEndpoint(ModelEndpoint.COMPLETIONS, super.getModel(), super.getEndpoint());
+        return new CompletionRequest()
+                .setEndpoint(finalEndpoint)
+                .setPrompt(prompt)
+                .setTemperature(temperature)
+                .setTopK(topK)
+                .setTopP(topP)
+                .setPenaltyScore(penaltyScore)
+                .setStop(stop)
+                .setUserId(super.getUserId())
+                .setExtraParameters(super.getExtraParameters());
+    }
+
+    public CompletionResponse execute() {
+        return super.getQianfan().completion(build());
+    }
+
+    public Iterator<CompletionResponse> executeStream() {
+        return super.getQianfan().completionStream(build());
+    }
+}

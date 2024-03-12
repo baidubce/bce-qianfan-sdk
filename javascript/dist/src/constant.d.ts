729dec1dbc7d0b5c277243e56976f35258fd0e85
wangting829 <1940087162@qq.com>
2024-03-12 18:40:38 +0800

    feat[js]: 打包优化，移除dist目录 (#331)
    * AIPD-DEV-21689 [TechFeature] 一言插件
    
    * AIPD-DEV-22219 [任务] 图生文
    
    * AIPD-DEV-22219 [任务] 图生文
    
    * AIPD-DEV-22219 [任务] 图生文
    
    * AIPD-DEV-22243 [任务] 【JS】打包优化，移除dist目录
    
    ---------
    
    Co-authored-by: wangting31 <wangting31@baidu.com>

--- javascript/dist/src/constant.d.ts
+++
@@ -1,9 +0,0 @@
-import { APIErrorCode } from './enum';
-export declare const BASE_HOST = "https://aip.baidubce.com";
-export declare const BASE_PATH = "/rpc/2.0/ai_custom/v1/wenxinworkshop";
-export declare const API_BASE: string;
-export declare const DEFAULT_HEADERS: {
-    'Content-Type': string;
-    Accept: string;
-};
-export declare const RETRYCODE: APIErrorCode[];

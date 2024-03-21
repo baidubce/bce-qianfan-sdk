0bfdc64d69c77619f558a0414a871de84526775f
guoweiming <guoweiming@baidu.com>
2024-03-21 18:01:57 +0800

    update ntbk_test

--- python/scripts/run_test_ntbk.sh
+++ python/scripts/run_test_ntbk.sh
@@ -1,35 +1,118 @@
 #!/bin/bash
 set -e
 
-function get_config()
-{
+declare func_call=
+declare reg=
+declare params=
+declare root_dir=
+declare env_file=
+declare QIANFAN_ACCESS_KEY=
+declare QIANFAN_SECRET_KEY=
+declare KEYWORDS_DICT=
+
+
+function usage() {
+    echo "sync github repo to iCode repo usage:"
+    echo "  $0 [-f <func_call>] [-r <path_reg>] [-p <param_dict>]"
+    echo ""
+    echo "options:"
+    echo "  -f function_call"
+    echo "  -r path_reg"
+    echo "  -p param_dict"
+    echo "  -a QIANFAN_ACCESS_KEY"
+    echo "  -s QIANFAN_SECRET_KEY"
+    echo "  -k KEYWORDS_DICT"
+    echo ""
+    echo "Example:"
+    echo "  $0 -f test_reg -r datasets"
+}
+
+function get_config(){
   param=$1
   value=$(sed -E '/^#.*|^ *$/d' "$env_file" | awk -F "${param}=" "/${param}=/{print \$2}" | tail -n1)
   echo "$value"
 }
 
-func_call=$1
-reg=$2
-params=$3
+function check_param() {
+    if [ -z "$1" ]; then
+        usage
+        exit
+    fi
+}
 
+function parse_param() {
+    while getopts :f:r:p:a:s:k:h opt; do
+        case $opt in
+            f)
+                func_call="$OPTARG"
+            ;;
+            r)
+                reg="$OPTARG"
+            ;;
+            p)
+                params="$OPTARG"
+            ;;
+            a)
+                check_param "$OPTARG"
+                QIANFAN_ACCESS_KEY="$OPTARG"
+            ;;
+            s)
+                check_param "$OPTARG"
+                QIANFAN_SECRET_KEY="$OPTARG"
+            ;;
+            k)
+                check_param "$OPTARG"
+                KEYWORDS_DICT="$OPTARG"
+            ;;
+            \h)
+                usage
+                exit
+            ;;
+            \?)
+                usage
+                exit
+            ;;
+        esac
+    done
+    if  [[ -z "$QIANFAN_ACCESS_KEY"  ||  -z "$QIANFAN_SECRET_KEY"  || -z "$KEYWORDS_DICT" ]]; then
+      echo "QIANFAN_ACCESS_KEY QIANFAN_SECRET_KEY or KEYWORDS_DICT isn't set"
+      exit 1
+    fi
+
+    if [[ ! -z $reg && -z $func_call ]]; then
+      func_call="test_reg"
+    fi
+
+    if [[ ! -z $reg && $func_call != "test_reg" ]]; then
+      echo "only test_reg supports -r"
+    fi
+
+    if [[ ! -z $params && $func_call != "test_reg" ]]; then
+      echo "only test_reg supports -p"
+      exit 1
+    fi
+
+    if [ ! -z $func_call ]; then
+      func_call="::$func_call"
+    fi
+}
+
+function load_env(){
+  QIANFAN_ACCESS_KEY=$(get_config QIANFAN_ACCESS_KEY)
+  QIANFAN_SECRET_KEY=$(get_config QIANFAN_SECRET_KEY)
+  KEYWORDS_DICT=$(get_config KEYWORDS_DICT)
+}
+
+################################
+## main
+################################
 root_dir=$(dirname "$0")/../..
 root_dir=$(realpath "$root_dir")
 env_file="$root_dir/.env"
 
-QIANFAN_ACCESS_KEY=$(get_config QIANFAN_ACCESS_KEY)
-QIANFAN_SECRET_KEY=$(get_config QIANFAN_SECRET_KEY)
-KEYWORDS_DICT=$(get_config KEYWORDS_DICT)
+load_env
 
-if  [[ -z "$QIANFAN_ACCESS_KEY"  ||  -z "$QIANFAN_SECRET_KEY"  || -z "$KEYWORDS_DICT" ]]; then
-  echo "QIANFAN_ACCESS_KEY QIANFAN_SECRET_KEY or KEYWORDS_DICT isn't set"
-  exit 1
-fi
-
-if [ ! -z $func_call ]; then
-  echo "test $func_call"
-  func_call="::$func_call"
-fi
-
+parse_param "$@"
 
 cd "$root_dir"/python/test_CI || exit 1
-poetry run coverage run -m  pytest -v -r A --full-trace  test_ntbk.py"$func_call" --ak "$QIANFAN_ACCESS_KEY" --sk "$QIANFAN_SECRET_KEY" --keywords "$KEYWORDS_DICT" --root-dir "$root_dir" --reg "\"$reg\"" --params "$params"
+poetry run coverage run -m  pytest -v -r A --full-trace  test_ntbk.py"$func_call" --ak "$QIANFAN_ACCESS_KEY" --sk "$QIANFAN_SECRET_KEY" --keywords "$KEYWORDS_DICT" --root-dir "$root_dir" --reg "$reg" --params "$params"

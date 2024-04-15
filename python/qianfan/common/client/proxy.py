ad76bf47425624c3dce9bb5d609a1fa9b21a3165
guoweiming <guoweiming@baidu.com>
2024-04-15 14:42:08 +0800

    feat[CLI]: add qianfan proxy

--- python/qianfan/common/client/proxy.py
+++ python/qianfan/common/client/proxy.py
@@ -11,32 +11,81 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import asyncio
 import json
 import logging
-from aiohttp import ClientSession, ClientResponse
-from typing import Any, AsyncIterator, Optional, Dict, Tuple, Union
+from typing import Any, AsyncIterator, Callable, Dict, Optional, Union
 
-import qianfan
-import requests
+from aiohttp import ClientSession
 from fastapi import FastAPI, Request
 from starlette.responses import JSONResponse, Response, StreamingResponse
 
-from qianfan.extensions.openai.adapter import OpenAIApdater
+import qianfan
+from qianfan.resources.auth.oauth import AuthManager
 from qianfan.utils.utils import get_ip_address
-from qianfan.resources.auth.oauth import AuthManager, Auth
 
 app = FastAPI()
 
-adapter = OpenAIApdater()
 auth = AuthManager()
+
+
+async def get_json_response(
+    url: str, body: bytes, query_params: Optional[dict] = None
+) -> Dict[str, Any]:
+    """
+    向指定的URL发送POST请求，并将返回的JSON响应体作为字典返回。
+
+    Args:
+        url (str): 请求的URL地址。
+        body (bytes): 发送的请求体数据。
+        query_params (Optional[dict], optional): 查询参数，默认为None。
+
+    Returns:
+        Dict[str, Any]: 响应体的JSON数据，以字典形式返回。
+
+    """
+    async with ClientSession() as session:
+        async with session.post(url, data=body, params=query_params) as resp:
+            resp_body = await resp.json()
+            logging.info(resp_body)
+            return resp_body
+
+
+async def get_json_response_async(
+    url: str, body: bytes, query_params: Optional[dict] = None
+) -> AsyncIterator[str]:
+    """
+    异步获取指定URL的流式响应，并将内容作为字符串返回。
+
+    Args:
+        url (str): 请求的URL。
+        body (bytes): 请求体，以字节形式传递。
+        query_params (Optional[dict], optional): 查询参数，以字典形式传递。默认为None。
+
+    Returns:
+        AsyncIterator[str]: 异步迭代器，用于返回解码后的JSON响应内容。
+
+    """
+    async with ClientSession() as session:
+        async with session.post(url, data=body, params=query_params) as resp:
+            async for data in resp.content:
+                logging.info(data)
+                yield data.decode("utf-8")
 
 
 async def get_response(
     token: str, request: Request
-) -> Union[Dict[str, Any], AsyncIterator[str]]:
+) -> Union[Dict[str, Any], AsyncIterator]:
     """
-    Get a new response from the server.
+    从api服务器获取新的响应。
+
+    Args:
+        token (str): 访问令牌。
+        request (Request): HTTP请求对象。
+
+    Returns:
+        Union[Dict[str, Any], AsyncIterator]:
+            如果响应体是流式传输的，则返回一个异步迭代器，否则返回一个包含响应体的字典。
+
     """
     base_url = request.base_url
 
@@ -53,32 +102,43 @@ async def get_response(
     assert body is not None
     is_stream = body.get("stream", False)
 
-    async with ClientSession() as session:
-        async with session.post(
-            url, data=json.dumps(body).encode("utf-8"), params=query_params
-        ) as resp:
-            if is_stream:
-                async for data in resp.content:
-                    logging.info(data)
-                    yield data
-            resp_body = await resp.json()
-            logging.info(resp_body)
-            yield resp_body
+    if is_stream:
+        return get_json_response_async(
+            url, json.dumps(body).encode("utf-8"), query_params
+        )
+    else:
+        return await get_json_response(
+            url, json.dumps(body).encode("utf-8"), query_params
+        )
 
 
 @app.middleware("http")
-async def add_token(request: Request, call_next):
+async def add_token(request: Request, call_next: Callable) -> Response:
+    """
+    中间件函数，用于向请求中添加访问令牌。
+
+    Args:
+        request (Request): 请求对象。
+        call_next (Callable): 调用下一个中间件的函数。
+
+    Returns:
+        Response: 处理后的响应对象。
+
+    Raises:
+        ValueError: 如果AK和SK未设置，或者获取访问令牌失败，则会抛出异常。
+    """
     config = qianfan.get_config()
     ak, sk = config.AK, config.SK
 
+    if ak is None or sk is None:
+        raise ValueError("AK and SK must be set.")
+
     auth.register(ak, sk)
     token = auth.get_access_token(ak, sk)
-
     if token is None:
         raise ValueError("Failed to get access token.")
 
-    resp = get_response(token, request)
-
+    resp = await get_response(token, request)
     if isinstance(resp, AsyncIterator):
         return StreamingResponse(resp, media_type="text/event-stream")
 
@@ -115,12 +175,6 @@ def entry(host: str, port: int, detach: bool, log_file: Optional[str]) -> None:
     if display_host == "0.0.0.0":
         display_host = get_ip_address()
     messages.append(f"- http://{display_host}:{port}")
-
-    # messages.append("\nRemember to set the environment variables:")
-    # messages.append(f"""```shell
-    # export OPENAI_API_KEY='any-content-you-want'
-    # export OPENAI_BASE_URL='http://{display_host}:{port}/v1'
-    # """)
 
     rich.print(Markdown("\n".join(messages)))
     rich.print()

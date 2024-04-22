8fa217db6139dd0220b3e1b22ba18d1402ebfe57
guoweiming <guoweiming@baidu.com>
2024-04-22 15:34:11 +0800

    fear[update]: proxy add iam

--- python/qianfan/common/client/proxy.py
+++ python/qianfan/common/client/proxy.py
@@ -11,14 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import AsyncIterator, Callable, Optional
+from typing import AsyncIterator, Optional
 
 from fastapi import FastAPI, Request
 from starlette.responses import JSONResponse, Response, StreamingResponse
 
+from qianfan.consts import DefaultValue
 from qianfan.extensions.proxy.proxy import ClientProxy
 from qianfan.utils.utils import get_ip_address
-from qianfan.consts import DefaultValue
 
 app = FastAPI()
 proxy = ClientProxy()
--- python/qianfan/extensions/proxy/proxy.py
+++ python/qianfan/extensions/proxy/proxy.py
@@ -1,15 +1,17 @@
+import logging
 from typing import Any, AsyncIterator, Dict, Union
+from urllib.parse import urlparse
 
 from aiohttp import ClientResponse
 from fastapi import Request
 
 from qianfan import get_config
 from qianfan.config import GlobalConfig
-from qianfan.resources.http_client import HTTPClient
+from qianfan.errors import InvalidArgumentError
 from qianfan.resources.auth.iam import iam_sign
 from qianfan.resources.auth.oauth import Auth
+from qianfan.resources.http_client import HTTPClient
 from qianfan.resources.typing import QfRequest
-from qianfan.consts import DefaultValue
 
 
 class ClientProxy(object):
@@ -20,6 +22,26 @@ class ClientProxy(object):
     def __init__(self) -> None:
         pass
 
+    def _sign(self, request: QfRequest) -> None:
+        """
+        对请求进行签名。
+        Args:
+            request (QfRequest): 请求对象。
+        """
+        if not self._auth._credential_available():
+            raise InvalidArgumentError(
+                "no enough credential found, any one of (access_key, secret_key),"
+                " (ak, sk), access_token must be provided"
+            )
+
+        url, path = request.url, urlparse(request.url).path
+
+        request.url = path
+        iam_sign(str(self._config.ACCESS_KEY), str(self._config.SECRET_KEY), request)
+        request.url = url
+        if not request.headers.get("Authorization", None):
+            request.query["access_token"] = self._auth.access_token()
+
     async def get_request(self, request: Request, url_route: str) -> QfRequest:
         """
         获取请求对象。
@@ -29,36 +51,27 @@ class ClientProxy(object):
         Returns:
             QfRequest: 请求对象。
         """
-        print(request.scope)
-
-        # 获取请求参数
-        query_params = {}
-        if request.scope["query_string"] != b"":
-            query_params = {
-                q.split("=")[0]: q.split("=")[1]
-                for q in request.scope["query_string"].decode("utf-8").split("&")
-            }
+        # 获取请求头
+        host = urlparse(url_route).netloc
+        headers = {
+            "Content-Type": "application/json",
+            "Host": host,
+        }
 
         # 获取请求url
-        if url_route is DefaultValue.BaseURL:
-            url = f"{DefaultValue.BaseURL}{request.scope['path'].replace('/base', '')}"
-        else:
-            url = (
-                f"{DefaultValue.ConsoleAPIBaseURL}{request.scope['path'].replace('/console', '')}"
-            )
-
-        # 获取请求头
-        headers = {
-            k.decode("utf-8"): v.decode("utf-8") for k, v in request.scope["headers"]
-        }
+        path = (
+            request.url.path.replace("/base", "")
+            if request.url.path.startswith("/base")
+            else request.url.path.replace("/console", "")
+        )
 
         # 获取请求体
         json_body = await request.json()
         return QfRequest(
-            url=url,
-            method=request.scope["method"],
+            url=url_route + path,
             headers=headers,
-            query=query_params,
+            method=request.method,
+            query=dict(request.query_params),
             json_body=json_body,
         )
 
@@ -77,7 +90,7 @@ class ClientProxy(object):
 
     async def get_response(
         self, request: Request, url_route: str
-    ) -> Union[Dict[str, Any], AsyncIterator]:
+    ) -> Union[AsyncIterator, Dict[str, Any]]:
         """
         从api服务器获取新的响应。
 
@@ -91,12 +104,11 @@ class ClientProxy(object):
         """
 
         qf_req = await self.get_request(request, url_route)
-        iam_sign(self._config.ACCESS_KEY, self._config.SECRET_KEY, qf_req)
-        qf_req.query["access_token"] = self._auth.access_token()
+        self._sign(qf_req)
+        logging.warning(f"request: {qf_req}")
 
         if qf_req.json_body.get("stream", False):
-            resp = self._client.arequest_stream(qf_req)
-            return self.get_stream(resp)
+            return self.get_stream(self._client.arequest_stream(qf_req))
         else:
             resp, session = await self._client.arequest(qf_req)
             async with session:

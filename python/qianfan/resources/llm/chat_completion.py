e11fe8b8c00156eb1c787f10b4ee0b63f32c5430
Liu Jun <liujun45@baidu.com>
2024-05-14 14:59:40 +0800

    fix system is ignored when truncating overlong msgs

--- python/qianfan/resources/llm/chat_completion.py
+++ python/qianfan/resources/llm/chat_completion.py
@@ -1473,6 +1473,8 @@ class ChatCompletion(BaseResource):
 
         if model_info.max_input_chars is not None:
             chars_limit = model_info.max_input_chars
+            if "system" in body:
+                chars_limit -= len(body["system"])
             msg_list = body["messages"]
             last_msg = msg_list[-1]
             cur_length = len(last_msg["content"]) if last_msg.get("content") else 0
@@ -1494,6 +1496,8 @@ class ChatCompletion(BaseResource):
 
         if model_info.max_input_tokens is not None:
             token_limit = model_info.max_input_tokens
+            if "system" in body:
+                token_limit -= Tokenizer.count_tokens(body["system"], mode="local")
             msg_list = body["messages"]
             last_msg = msg_list[-1]
             cur_length = (

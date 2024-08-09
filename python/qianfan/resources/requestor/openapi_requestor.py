5b1fb6ed009581e1e915971812649fd84d9e7549
Dobiichi-Origami <56953648+Dobiichi-Origami@users.noreply.github.com>
2024-08-09 18:49:43 +0800

    为 do 和 ado 方法增加 `show_total_latency` 参数 (#724)
    * do 和 ado 增加 show_total_latency 函数
    
    * 格式化
    
    * 文档修改

--- python/qianfan/resources/requestor/openapi_requestor.py
+++ python/qianfan/resources/requestor/openapi_requestor.py
@@ -408,6 +408,7 @@ class QfAPIRequestor(BaseAPIRequestor):
         stream: bool = False,
         data_postprocess: Callable[[QfResponse], QfResponse] = lambda x: x,
         retry_config: RetryConfig = RetryConfig(),
+        show_total_latency: bool = False,
     ) -> Union[QfResponse, Iterator[QfResponse]]:
         """
         llm related api request
@@ -432,11 +433,34 @@ class QfAPIRequestor(BaseAPIRequestor):
             token_count = self._get_token_count_from_body(body)
             self._token_limiter.decline(token_count)
 
+            def _generator_wrapper(
+                generator: Iterator[QfResponse],
+            ) -> Iterator[QfResponse]:
+                for res in generator:
+                    if not show_total_latency:
+                        res.statistic["total_latency"] = 0
+                        res.statistic["request_latency"] = 0
+
+                    yield res
+
+            def _list_generator(data: List) -> Any:
+                for res in data:
+                    yield res
+
             if stream:
-                return self._compensate_token_usage_stream(
+                generator = self._compensate_token_usage_stream(
                     self._request_stream(req, data_postprocess=data_postprocess),
                     token_count,
                 )
+
+                if not show_total_latency:
+                    return _generator_wrapper(generator)
+                else:
+                    result_list: List[QfResponse] = []
+                    for res in generator:
+                        result_list.append(res)
+
+                    return _list_generator(result_list)
             else:
                 return self._compensate_token_usage_non_stream(
                     self._request(
@@ -457,6 +481,7 @@ class QfAPIRequestor(BaseAPIRequestor):
         stream: bool = False,
         data_postprocess: Callable[[QfResponse], QfResponse] = lambda x: x,
         retry_config: RetryConfig = RetryConfig(),
+        show_total_latency: bool = False,
     ) -> Union[QfResponse, AsyncIterator[QfResponse]]:
         """
         llm related api request
@@ -467,6 +492,22 @@ class QfAPIRequestor(BaseAPIRequestor):
                 if not m.get("name", None):
                     m["name"] = m.get("tool_call_id", "")
                     m.pop("tool_call_id", None)
+
+        class AsyncListIterator:
+            def __init__(self, data: List[QfResponse]):
+                self.data = data
+                self.index = 0
+
+            def __aiter__(self) -> "AsyncListIterator":
+                return self
+
+            async def __anext__(self) -> Any:
+                if self.index < len(self.data):
+                    value = self.data[self.index]
+                    self.index += 1
+                    return value
+                else:
+                    raise StopAsyncIteration
 
         @self._async_retry_if_token_expired
         async def _helper() -> Union[QfResponse, AsyncIterator[QfResponse]]:
@@ -481,13 +522,32 @@ class QfAPIRequestor(BaseAPIRequestor):
             token_count = self._get_token_count_from_body(body)
             await self._async_token_limiter.decline(token_count)
 
+            async def _async_generator_wrapper(
+                generator: AsyncIterator[QfResponse],
+            ) -> AsyncIterator[QfResponse]:
+                async for res in generator:
+                    if not show_total_latency:
+                        res.statistic["total_latency"] = 0
+                        res.statistic["request_latency"] = 0
+
+                    yield res
+
             if stream:
-                return self._async_compensate_token_usage_stream(
+                generator = self._async_compensate_token_usage_stream(
                     await self._async_request_stream(
                         req, data_postprocess=data_postprocess
                     ),
                     token_count,
                 )
+
+                if not show_total_latency:
+                    return _async_generator_wrapper(generator)
+                else:
+                    result_list: List[QfResponse] = []
+                    async for res in generator:
+                        result_list.append(res)
+
+                    return AsyncListIterator(result_list)
             else:
                 return await self._async_compensate_token_usage_non_stream(
                     await self._async_request(req, data_postprocess=data_postprocess),
@@ -734,6 +794,7 @@ class PrivateAPIRequestor(QfAPIRequestor):
         stream: bool = False,
         data_postprocess: Callable[[QfResponse], QfResponse] = lambda x: x,
         retry_config: RetryConfig = RetryConfig(),
+        show_total_latency: bool = False,
     ) -> Union[QfResponse, Iterator[QfResponse]]:
         """
         llm related api request

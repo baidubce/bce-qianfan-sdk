1c5b2cff99d40edc187c67d1ebeefffe1b35744d
zhonghanjun <zhonghanjun@baidu.com>
2024-08-17 15:53:57 +0800

    fix: merge

--- python/qianfan/__init__.py
+++ python/qianfan/__init__.py
@@ -29,7 +29,7 @@ from qianfan.fake_pyarrow_replacer import _ModuleFinder
 
 sys.meta_path.insert(0, _ModuleFinder())
 
-from qianfan.config import AK, SK, AccessKey, Token, SecretKey, get_config
+from qianfan.config import AK, SK, AccessKey, AccessToken, SecretKey, get_config
 from qianfan.resources import (
     ChatCompletion,
     Completion,

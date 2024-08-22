7d395bd9b01244c2b746db311923d404c4fb56f2
Dobiichi-Origami <56953648+Dobiichi-Origami@users.noreply.github.com>
2024-08-22 17:50:26 +0800

    增加 redis 依赖项

--- python/qianfan/resources/rate_limiter/__init__.py
+++ python/qianfan/resources/rate_limiter/__init__.py
@@ -1,7 +1,10 @@
 from qianfan.resources.rate_limiter.base_rate_limiter import BaseRateLimiter
 from qianfan.resources.rate_limiter.rate_limiter import VersatileRateLimiter
+from qianfan.resources.rate_limiter.redis_rate_limiter import RedisRateLimiter, RedisConnectionInfo
 
 __all__ = [
     "BaseRateLimiter",
     "VersatileRateLimiter",
+    "RedisRateLimiter",
+    "RedisConnectionInfo"
 ]

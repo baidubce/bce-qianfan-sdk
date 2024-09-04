2a4ff71537d3b4e4ebeaf4ecbb7bff4540da2bb9
Dobiichi-Origami <56953648+Dobiichi-Origami@users.noreply.github.com>
2024-09-04 14:51:33 +0800

    格式化

--- python/qianfan/resources/rate_limiter/base_rate_limiter.py
+++ python/qianfan/resources/rate_limiter/base_rate_limiter.py
@@ -1,30 +1,14 @@
 import abc
-from typing import Any, AsyncContextManager, ContextManager, Optional
+from typing import AsyncContextManager, ContextManager, Optional
 
 
-class BaseRateLimiter(abc.ABC, ContextManager, AsyncContextManager):
+class BaseRateLimiter(ContextManager, AsyncContextManager):
     @abc.abstractmethod
     def reset_once(self, rpm: float) -> None:
         ...
 
     @abc.abstractmethod
     async def async_reset_once(self, rpm: float) -> None:
-        ...
-
-    @abc.abstractmethod
-    def __enter__(self) -> None:
-        ...
-
-    @abc.abstractmethod
-    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
-        ...
-
-    @abc.abstractmethod
-    async def __aenter__(self) -> None:
-        ...
-
-    @abc.abstractmethod
-    async def __aexit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
         ...
 
     @abc.abstractmethod
